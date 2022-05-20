5ade89c71a2f46563b604c7f87dd9ada52950629
cmotu <cmotu@sandiego.edu>
2022-05-19 17:18:27 -0700

    Added CodeSandbox React Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import WeatherSearch from './WeatherSearch';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!!</h1>
+        <WeatherSearch />
       </header>
     </div>
   );
