74f5227b1c0afe209fcecc6f7e5fa7771fa85c94
cmotu <cmotu@sandiego.edu>
2022-05-29 14:55:45 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import WeatherSearch from './WeatherSearch';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <WeatherSearch />
         <a href="https://github.com/cmotu/weather-react">Open-Sourced Code by Christina Motu</a>
       </header>
+    </div>
     </div>
   );
 }
